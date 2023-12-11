8f397be3e850a519eab3cc1dd0ed07773ea45b61
drewsolis117@gmail.com <drewsolis117@gmail.com>
2023-12-11 15:57:09 -0800

    commit three

--- shoppinglist02.js
+++ shoppinglist02.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const ClearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 function addItem(e) {
     e.preventDefault();
@@ -22,7 +24,9 @@ function addItem(e) {
     
     itemList.appendChild(li);
 
-itemInput.value = '';
+    checkUI();
+
+    itemInput.value = '';
 }
 
 //This creates the delete buttons for list items 
@@ -34,11 +38,44 @@ function createButton(classes) {
     return button;
 }
 
+//This creates the icons for the delete button 
 function createIcon(classes) {
     const icon = document.createElement('i');
     icon.className = classes;
     return icon 
 }
 
+//This deletes an item in the list when the delete button is clicked 
+function removeItem(e) {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+       if (confirm('Are you sure?')) {
+        e.target.parentElement.parentElement.remove();
+
+        checkUI();
+       }
+    }
+}
+
+//When the clear all button is clicked, all items are deleted 
+function clearItems() {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+    checkUI();
+}
+
+function checkUI() {
+    const items = itemList.querySelectorAll('li');
+    if (itemList.length === 0) {
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
+}
+
 //Event Listeners Below 
-itemForm.addEventListener('submit',addItem);
+itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+ClearBtn.addEventListener('click', clearItems);
